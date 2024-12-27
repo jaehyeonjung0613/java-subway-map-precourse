776e0e5b71a10dff0808bead63a5876e7e304649
jaehyeonjung0613 <jaehyeonjung0613@gmail.com>
2024-12-27 11:56:01 +0900

    feat: 9. View 동작 구현 - [View.java] 동작 구현 - [ViewConstants.java] View 상수 클래스 생성, 메뉴 선택 질의문 정의

--- src/main/java/subway/view/View.java
+++ src/main/java/subway/view/View.java
@@ -1,27 +1,39 @@
 package subway.view;
 
+import static subway.view.ViewConstants.*;
+
+import subway.menu.Menu;
+import subway.ui.Console;
+
 public class View {
     private final String name;
+    private final Menu menu;
 
-    public View(String name) {
+    public View(String name, Menu menu) {
         this.name = name;
-    }
-
-    public void show() {
-    }
-
-    public String requestCommand() {
-        return null;
-    }
-
-    public void onSelect(String command) {
-    }
-
-    public boolean isClose() {
-        return true;
+        this.menu = menu;
     }
 
     public String getName() {
         return name;
+    }
+
+    public void show() {
+        Console.printHeader(this.getName());
+        Console.println(this.menu.outputAll());
+    }
+
+    public String requestCommand() {
+        Console.printHeader(REQUEST_COMMAND_QUERY);
+        return Console.readline();
+    }
+
+    public void onSelect(String command) {
+        Console.printNextLine();
+        this.menu.select(command);
+    }
+
+    public boolean isClose() {
+        return this.menu.isClose();
     }
 }
---
+++ src/main/java/subway/view/ViewConstants.java
@@ -0,0 +1,8 @@
+package subway.view;
+
+public final class ViewConstants {
+	private ViewConstants() {
+	}
+
+	public static final String REQUEST_COMMAND_QUERY = "원하는 기능을 선택하세요.";
+}
