0f5aab0ea61ec80817ada9db13bcb429bbabf707
jaehyeonjung0613 <jaehyeonjung0613@gmail.com>
2024-12-31 17:45:00 +0900

    fix, docs: 26. 노선 관리 유효성 체크 - [solving_process.md] 풀이 수정 - [LineConstants.java] 역 → 노선 변경

--- src/main/java/subway/domain/LineConstants.java
+++ src/main/java/subway/domain/LineConstants.java
@@ -6,6 +6,6 @@ public final class LineConstants {
 
     public static final int MIN_NAME_LENGTH = 2;
 
-    public static final String EMPTY_NAME_MESSAGE = "지하철 역 이름은 필수로 입력되어야합니다.";
-    public static final String LESS_NAME_LENGTH_FORMAT = "지하철 역 이름은 %d글자 이상이어야 합니다.";
+    public static final String EMPTY_NAME_MESSAGE = "지하철 노선 이름은 필수로 입력되어야합니다.";
+    public static final String LESS_NAME_LENGTH_FORMAT = "지하철 노선 이름은 %d글자 이상이어야 합니다.";
 }
