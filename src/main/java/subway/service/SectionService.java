d7d17672185fa7274baa271f6705fe56b384643a
jaehyeonjung0613 <jaehyeonjung0613@gmail.com>
2025-01-03 14:21:18 +0900

    fix, docs: 30. 구간 관리 유효성 체크 - [solving_process.md] 풀이 수정 - [SectionService.java] Line, Station 미존재 메시지 수정

--- src/main/java/subway/service/SectionService.java
+++ src/main/java/subway/service/SectionService.java
@@ -12,17 +12,17 @@ import subway.repository.StationRepository;
 public class SectionService implements Service {
     public void insertSection(LineDTO lineDTO, StationDTO stationDTO, int position) throws IllegalArgumentException {
         Line line = LineRepository.selectByName(lineDTO.getName())
-            .orElseThrow(() -> new IllegalArgumentException(NOT_EXISTS_STATION_NAME_MESSAGE));
-        Station station = StationRepository.selectByName(stationDTO.getName())
             .orElseThrow(() -> new IllegalArgumentException(NOT_EXISTS_LINE_NAME_MESSAGE));
+        Station station = StationRepository.selectByName(stationDTO.getName())
+            .orElseThrow(() -> new IllegalArgumentException(NOT_EXISTS_STATION_NAME_MESSAGE));
         line.insertStation(position, station);
     }
 
     public void deleteSection(LineDTO lineDTO, StationDTO stationDTO) throws IllegalArgumentException {
         Line line = LineRepository.selectByName(lineDTO.getName())
-            .orElseThrow(() -> new IllegalArgumentException(NOT_EXISTS_STATION_NAME_MESSAGE));
-        Station station = StationRepository.selectByName(stationDTO.getName())
             .orElseThrow(() -> new IllegalArgumentException(NOT_EXISTS_LINE_NAME_MESSAGE));
+        Station station = StationRepository.selectByName(stationDTO.getName())
+            .orElseThrow(() -> new IllegalArgumentException(NOT_EXISTS_STATION_NAME_MESSAGE));
         line.removeStation(station);
     }
 
